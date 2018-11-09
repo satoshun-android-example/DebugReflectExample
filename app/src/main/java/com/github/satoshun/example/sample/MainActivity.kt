76add76573d2d9609fda6f68aeccec2078103695
SatoShun <shun.sato1@gmail.com>
2018-11-09 23:58:04 +0000

    first

---
+++ app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.github.satoshun.example.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+class MainActivity : AppCompatActivity(),
+  CoroutineScope {
+
+  private val job = Job()
+  override val coroutineContext: CoroutineContext get() = job + Dispatchers.Main
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    job.cancel()
+  }
+}
